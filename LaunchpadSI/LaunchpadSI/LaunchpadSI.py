84a8a0075570861522d11cdd4d485914ba98159d
NeophytePon4 <zakalex.body@gmail.com>
2018-01-13 16:09:40 +0000

    Improved Words

--- LaunchpadSI/LaunchpadSI/LaunchpadSI.py
+++ LaunchpadSI/LaunchpadSI/LaunchpadSI.py
@@ -49,8 +49,9 @@ class LaunchpadMK2:
 
         sleep(time)
         #self.multiOffXY(letter)
-        self.reset()
+        #self.reset()
     
+    #Be warned! This is the most evil function in programming history...
     def scrollWord(self, letter, color=36, time=0):
 
         #If the word is a single letter
@@ -73,29 +74,42 @@ class LaunchpadMK2:
         #If the word is more than 1
         #Word
         elif len(letter) > 1:
-            letters = [[] for x in range(len(letter))]
+            
 
             letter = self.getWord(letter)
             
-            for i in range(len(letter)):
-                for n in range(len(letter[i])):
-                    letter[i][n][1] += 9 * i
-            
+            d = letter
+            with open("Debug.json", "w") as outfile:
+                json.dump(d, outfile)
+            for i in range(len(d)):
+                for n in range(len(d[i])):
+                    d[i][n][1] += 9 * i
+                    print(i)
 
-            for i in range(10 * len(letters)):
+            for i in range(10 * len(letter)):
+
                 for n in range(len(letter)):
-                    try:
-                    
-                        self.printLetter(letter[n], time=1)
+                    for x in range(len(letter[n])):
+                        if letter[n][x][1] >= 0:
+                            positive = True
+                        else:
+                            
+                            positive = False
+                    if positive:
+                        try:
                         
-                    except IndexError:
-                        pass
+                            self.printLetter(letter[n], time=0.1)
+                            
+                        except IndexError:
+                            pass
 
                 for n in range(len(letter)):
                     for x in range(len(letter[n])):
                         letter[n][x][1] -= 1
-                        if letter [n][x][1] < -1:
-                            letter[n][x][1] = -25
+                        if letter[n][x][1] < 0:
+                            letter[n][x][1] = -100
+                        
+                self.reset()
                             
 
     @staticmethod
@@ -107,18 +121,19 @@ class LaunchpadMK2:
     @staticmethod
     def getWord(word):
         chars = json.load(open("Chars.json"))
+        
         letters = chars["letters"]
         outp = [[] for x in range(len(word))]
 
         for i in range(len(word)):
             outp[i] = letters[word[i].upper()]
-
+        
         return outp
 
 if __name__ == "__main__":
     lp = LaunchpadMK2(1, 3)
     #lp.printLetter("A")
-    lp.scrollWord("Range")
+    lp.scrollWord("Zak")
 
 
     lp.reset()
